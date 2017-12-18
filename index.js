314cca884e4b769fbc5b29e5906ab88871197c81
Jeff Hill <34660638+HappeyMemeMan@users.noreply.github.com>
2017-12-18 20:42:53 +0000

    Update index.js

--- index.js
+++ index.js
@@ -275,6 +275,7 @@ bot.on("message", function(message) {//Aqui é que o bot começa a trabalhar com
 			case "Delet":
 				message.delete();
 				message.channel.send("D-DELETE THIS!", {file:﻿"https://i.imgur.com/xga6glR.jpg"});
+				userData.points= userData.points - 1;
 				break;
 			case "Play":
 				if(!args[1]) {
