0d2b772cce34ead774ab000cabfd600a3800907d
Matsuni <34627629+Matsuni@users.noreply.github.com>
2018-05-25 12:29:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -356,6 +356,9 @@ bot.on("message", function(message) {//Aqui é que o bot começa a trabalhar com
 					}
 				else{ message.channel.send("G-Goodnight (~˘▾˘)~");}
 				break;
+			case "profpic":
+				message.author.avatarURL;
+				break;
 			case "play":
 				if(!args[1]) {
 					message.channel.sendMessage("I-I need a link... please...!");
