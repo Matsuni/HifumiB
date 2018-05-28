7a52698096d14eeba466eddf7fc7ad4c5d7b6bd4
Matsuni <34627629+Matsuni@users.noreply.github.com>
2018-05-28 10:57:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -235,7 +235,7 @@ bot.on("message", function(message) {//Aqui é que o bot começa a trabalhar com
 				var embed = new Discord.RichEmbed()
 					.addField("Level: ", userData.level)
 					.addField("HifumiPoints: ", userData.points)
-					.addField("Role: ", message.guild.roles.get.members)
+					.addField("Role: ", message.member.roles.has(role.id))
 					.setColor(132344)
 					.setThumbnail(message.author.avatarURL)
 				message.channel.sendEmbed(embed);
