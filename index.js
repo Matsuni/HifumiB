30947ee7e516d13768f7caf73f8572c48fcd28df
Matsuni <34627629+Matsuni@users.noreply.github.com>
2018-05-30 14:13:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -347,7 +347,7 @@ bot.on("message", function(message) {//Aqui é que o bot começa a trabalhar com
 			case "shock":
 				message.delete();
 				message.channel.send(message.author.toString() + " is shocked.");
-				message.channel.send({file:﻿"https://i.pinimg.com/originals/d3/d2/02/d3d202c451b013f2f7cb40aa67d3a4ba.gif");
+				message.channel.send({file:﻿"https://i.pinimg.com/originals/d3/d2/02/d3d202c451b013f2f7cb40aa67d3a4ba.gif"});
 				break;
 			case "Goodnight":
 				if (new Date().getHours() < 18) {
